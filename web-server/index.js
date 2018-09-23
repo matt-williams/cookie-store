a1a3259d70470184fbbac997dbbcb563343af414
Matt Williams <matwilliams@hotmail.com>
2018-09-23 05:50:31 +0000

    Support virtual hosts to make demo easier

--- web-server/index.js
+++ web-server/index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const uuid = require('uuid/v4');
+const mustacheExpress = require('mustache-express');
+const cookieSession = require('cookie-session');
+const crypto = require('crypto');
 const app = express();
 const port = process.env.PORT || 8080;
-const server = process.env.SERVER || "server1";
+
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use(cookieSession({
+  name: 'session',
+  secret: 'secret', // TODO Be secure
+  maxAge: 30 * 60 * 1000
+}));
 
 app.get('/', (req, res) => {
-  res.set('X-CookieStore', `${server} ${uuid()}`);
-  res.send('Hello World!');
+  if (!req.session.isPopulated) {
+    req.session.uid = uuid();
+  }
+  var host = req.get('Host');
+  var server = host.split('.')[0];
+
+  var color = '#' + crypto.createHash('md5').update(host).digest("hex").substring(0, 6) + '7f';
+
+  res.set('X-CookieStore', `${server} ${req.session.uid}`);
+  res.render('index.mustache', {server: server, host: host, uid: req.session.uid, color: color});
 })
 
 app.use(express.static('static'));
