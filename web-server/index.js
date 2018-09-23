e529fb620d521fc8dc452207e2f5940bc531dfd4
Matt Williams <matwilliams@hotmail.com>
2018-09-23 02:17:28 +0000

    Fix server typos

--- web-server/index.js
+++ web-server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express(;)
+const app = express();
 const port = process.env.PORT || 8080;
 const server = process.env.SERVER || "server1";
 
 app.get('/', (req, res) => {
-  res.set('X-CookieStore', '${server} 123456789abcdef');
-  res.send('Hello World!')
+  res.set('X-CookieStore', `${server} 123456789abcdef`);
+  res.send('Hello World!');
 })
 
 app.use(express.static('static'));
