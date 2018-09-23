6b17ffb79d5c348a490e785723ae7130659dc263
Matt Williams <matwilliams@hotmail.com>
2018-09-23 02:48:07 +0000

    Basic demo function working

---
+++ chrome-extension/options.js
@@ -0,0 +1,39 @@
+const PRIVATE_KEY_UNKNOWN = "***************************************************";
+
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.sync.get({
+    account: "",
+    publicKey: ""
+  }, (items) => {
+    document.getElementById('account').value = items.account;
+    document.getElementById('publicKey').value = items.publicKey;
+    if (items.publicKey) {
+      document.getElementById('privateKey').value = PRIVATE_KEY_UNKNOWN;
+    } else {
+      document.getElementById('privateKey').value = "";
+    }
+  });
+});
+
+document.getElementById('publicKey').addEventListener('onchange', () => {
+  if (privateKey.value == PRIVATE_KEY_UNKNOWN) {
+    privateKey.value = "";
+  }
+});
+
+document.getElementById('save').addEventListener('click', () => {
+  var account = document.getElementById('account').value;
+  var publicKey = document.getElementById('publicKey').value;
+  var privateKey = document.getElementById('privateKey').value;
+  var status = document.getElementById('status');
+  var data = {account: account, publicKey: publicKey};
+  if (privateKey != PRIVATE_KEY_UNKNOWN) {
+    data.privateKey = privateKey;
+  }
+  chrome.storage.sync.set(data, () => {
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 1000);
+  });
+});
